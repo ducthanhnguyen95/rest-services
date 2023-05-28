7fcba678497f9bead352c3b0e7f66f44a428a977
thanhnd <ducthanhnguyen95@gmail.com>
2023-05-28 22:46:41 +0700

    Managing the HTTP response - Setting the response status and headers

--- src/main/java/com/example/rest/services/controller/CountryController.java
+++ src/main/java/com/example/rest/services/controller/CountryController.java
@@ -1,6 +1,8 @@
 package com.example.rest.services.controller;
 
 import com.example.rest.services.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +11,21 @@ import java.util.List;
 @RestController
 public class CountryController {
 
-    @GetMapping("/france")
-    public Country france() {
-        Country c = Country.of("France", 67);
-        return c;
-    }
-
     @GetMapping("/all")
     public List<Country> countries() {
         Country c1 = Country.of("France", 67);
         Country c2 = Country.of("Spain", 47);
         return List.of(c1, c2);
+    }
+
+    @GetMapping("/france")
+    public ResponseEntity<Country> france() {
+        Country c = Country.of("France", 67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
     }
 }
