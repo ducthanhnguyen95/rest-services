d33c3efeaf1d97d561084746412f2c79b4adbd95
thanhnd <ducthanhnguyen95@gmail.com>
2023-05-28 23:33:37 +0700

    Managing the HTTP response - Managing exceptions at the endpoint level - Use @RestControllerAdvice

---
+++ src/main/java/com/example/rest/services/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.example.rest.services.controller;
+
+import com.example.rest.services.exceptions.NotEnoughMoneyException;
+import com.example.rest.services.model.ErrorDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment.");
+        return ResponseEntity.badRequest().body(errorDetails);
+    }
+}
--- src/main/java/com/example/rest/services/controller/PaymentController.java
+++ src/main/java/com/example/rest/services/controller/PaymentController.java
@@ -1,7 +1,5 @@
 package com.example.rest.services.controller;
 
-import com.example.rest.services.exceptions.NotEnoughMoneyException;
-import com.example.rest.services.model.ErrorDetails;
 import com.example.rest.services.model.PaymentDetails;
 import com.example.rest.services.services.PaymentService;
 import org.springframework.http.HttpStatus;
@@ -19,15 +17,9 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails = paymentService.processPayment();
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity.badRequest().body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
     }
 }
 
